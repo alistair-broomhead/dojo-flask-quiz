6cce778d7098eeb59eb243329750fc9131b3e149
Alistair Broomhead <alistair.broomhead@gmail.com>
2016-02-05 12:47:35 +0000

    Show & tell time changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ quiz = tuple(
         "correct_answer",
         "incorrect answer",
         "incorrect answer",
-        "incorrect answer"
+        "incorrect answer",
     ) for i in range(number_of_questions)
 )
 
@@ -30,12 +30,25 @@ def root():
         answers=answers
     )
 
+
 @app.route("/", methods=["POST"])
 def answer():
     form = flask.request.form
-    from json import dumps
-    return dumps(form)
+    answers = list(form.items())
+    # We assume that there is only one answer for now
+    question_number, given_answer = answers[0]
+    question_number = int(question_number)
+
+    question, correct_answer, *incorrect_answers = quiz[question_number]
+
+    return render_template(
+        'result.html',
+        question_number=question_number,
+        question=question,
+        correct_answer=correct_answer,
+        given_answer=given_answer,
+    )
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
